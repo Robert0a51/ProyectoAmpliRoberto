d2492c912a11c10d40d92e2a800f6e29fb11bd40
Robert <robertogithubcanaveral@gmail.com>
2026-05-12 09:59:00 +0200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -13,4 +13,6 @@ sleep 10
 
 # MOVER EL PHP
 sudo docker cp index.php servidor_wp:/var/www/html/index.php
+# Dar permisos al archivo php así evitamos el Error 500
+sudo docker exec servidor_wp chmod 644 /var/www/html/index.php
 echo "Hecho. Mira en localhost:8080/"
